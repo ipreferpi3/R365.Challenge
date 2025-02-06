8025ace9126c194a65bdd106b3db40a34b3bf34d
Sean <shandley84@gmail.com>
2025-02-06 14:36:53 -0600

    Small fix regarding parsing new line character

--- R365.Challenge.Tests/DelimiterParserServiceTests.cs
+++ R365.Challenge.Tests/DelimiterParserServiceTests.cs
@@ -24,19 +24,19 @@ namespace R365.Challenge.Tests
             Assert.That(calculation.Delimiters, Is.Not.Empty);
             Assert.That(calculation.Delimiters.Count, Is.EqualTo(2));
             Assert.That(calculation.Delimiters.Contains(","), Is.True);
-            Assert.That(calculation.Delimiters.Contains("\n"), Is.True);
+            Assert.That(calculation.Delimiters.Contains("\\n"), Is.True);
         }
 
         [Test]
         public void GetDelimiters_ShouldReturnCustomAndDefaultDelimiters_WhenInputContainsCustomDelimiter()
         {
-            var input = "//#\n2#5";
+            var input = "//#\\n2#5";
             var calculation = _delimiterParserService.GetDelimiters(input);
 
             Assert.That(calculation.Delimiters, Is.Not.Empty);
             Assert.That(calculation.Delimiters.Count, Is.EqualTo(3));
             Assert.That(calculation.Delimiters.Contains(","), Is.True);
-            Assert.That(calculation.Delimiters.Contains("\n"), Is.True);
+            Assert.That(calculation.Delimiters.Contains("\\n"), Is.True);
             Assert.That(calculation.Delimiters.Contains("#"), Is.True);
         }
 
